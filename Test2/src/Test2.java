e0f2f0005abb2bf7f43eb6af9778aa45d9a1e9c8
TH1284 <qlwlel224@naver.com>
2019-03-11 11:59:32 +0900

    Signed-off-by: TH1284 <qlwlel224@naver.com>

--- Test2/src/Test2.java
+++ Test2/src/Test2.java
@@ -12,5 +12,15 @@ public class Test2 {
 		String inputNumber2 = s.next();
 		System.out.println("입력 한글");
 		System.out.println(inputNumber2);
+		
+		System.out.println("화면에 숫자를 입력해주세요!");
+		int inputNumber3 = s.nextInt();
+		System.out.print("입력할 숫자는");
+		System.out.println(inputNumber3+10);
+		System.out.println("화면에 숫자를 입력해주세요!");
+		int inpuNumber4 = s.nextInt();
+		int inpuNumber5 = s.nextInt();
+		System.out.print("입력할 숫자는");
+		System.out.println(inpuNumber4*inpuNumber5);
 	}
 }
