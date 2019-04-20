a1d8d895990899b2595455a22f4e839e0760ddac
TH1284 <qlwlel224@naver.com>
2019-04-20 17:50:11 +0900

    Signed-off-by: TH1284 <qlwlel224@naver.com>

---
+++ 객체 테스트/ObjectTest.java
@@ -0,0 +1,9 @@
+
+public class ObjectTest {
+	public static void main(String[] args) {
+		SumNumber a = new SumNumber();
+		
+		a.doSum();
+	}
+	
+}
---
+++ 여제문제 풀어보기/src/MainClassProb.java
@@ -0,0 +1,7 @@
+
+public class MainClassProb {
+	public static void main(String[] args) {
+		DoubleForProb a = new DoubleForProb();
+		a.doChek();
+	}
+}
